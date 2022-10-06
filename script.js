6c4800599356d6bbce9997b0cbf252c178738b23
gianmarcozizzo <zizzo.1663195@studenti.uniroma1.it>
2022-10-06 13:01:45 +0200

    si

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 var NATALE = ["Natale", "Dec 25, 2022 00:00:00"]
 var COMPLEANNO = ["Compleanno", "Feb 7, 2023 00:00:00"]
 var CAPODANNO = ["Capodanno", "Jan 1, 2023 00:00:00"]
+var COMPLEANNO_VECIENZO = ["Vincenzo", "Jun 11, 2023 00:00:00"]
+var COMPLEANNO_SPAGNOLO = ["Spagnolo", "May 10, 2023 00:00:00"]
+var COMPLEANNO_PEDONE = ["Pedone", "Jun 14, 2023 00:00:00"]
+var COMPLEANNO_GIARDINO = ["Giardino", "May 9, 2023 00: 00: 00"]
 
 var countDownDate1 = new Date(NATALE[1]).getTime();
 var countDownDate2 = new Date(COMPLEANNO[1]).getTime();
 var countDownDate3 = new Date(CAPODANNO[1]).getTime();
 
+var countDownDate4 = new Date(COMPLEANNO_VECIENZO[1]).getTime();
+var countDownDate5 = new Date(COMPLEANNO_SPAGNOLO[1]).getTime();
+var countDownDate6 = new Date(COMPLEANNO_PEDONE[1]).getTime();
+var countDownDate7 = new Date(COMPLEANNO_GIARDINO[1]).getTime();
+
+
 var Natale = document.getElementById(NATALE[0])
 var Compleanno = document.getElementById(COMPLEANNO[0])
 var Capodanno = document.getElementById(CAPODANNO[0])
+
+var Vincenzo = document.getElementById(COMPLEANNO_VECIENZO[1])
+var Spagnolo = document.getElementById(COMPLEANNO_SPAGNOLO[1])
+var Pedone = document.getElementById(COMPLEANNO_PEDONE[1])
+var Giardino = document.getElementById(COMPLEANNO_GIARDINO[1])
 
 function countdown(finish_date, timer) {
 
@@ -36,3 +51,8 @@ function countdown(finish_date, timer) {
 countdown(countDownDate1, Natale)
 countdown(countDownDate2, Compleanno)
 countdown(countDownDate3, Capodanno)
+
+countdown(countDownDate4, Vincenzo)
+countdown(countDownDate5, Spagnolo)
+countdown(countDownDate6, Pedone)
+countdown(countDownDate7, Giardino)
