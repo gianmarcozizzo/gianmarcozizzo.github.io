59b6624a7bc268957082ced545007d83a3d14293
gianmarcozizzo <zizzo.1663195@studenti.uniroma1.it>
2022-10-06 12:29:03 +0200

    si

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var countDownDate1 = new Date(NATALE[1]).getTime();
 var countDownDate2 = new Date(COMPLEANNO[1]).getTime();
 var countDownDate3 = new Date(CAPODANNO[1]).getTime();
 
-var Natale = document.getElementById("Natale")
+var Natale = document.getElementById(NATALE[0])
 var Compleanno = document.getElementById("Compleanno")
 var Capodanno = document.getElementById("Capodanno")
 
