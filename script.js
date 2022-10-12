878f35b60e6848b0fbb5802a0fb1ef721f213fe7
gianmarcozizzo <zizzo.1663195@studenti.uniroma1.it>
2022-10-12 11:45:32 +0200

    week

--- script.js
+++ script.js
@@ -2,31 +2,17 @@ var NATALE = ["Natale", "Dec 25, 2022 00:00:00"]
 var COMPLEANNO = ["Compleanno", "Feb 7, 2023 00:00:00"]
 var TRENTAANNI = ["Trentaanni", "Feb 7, 2025 00:00:00"]
 var CAPODANNO = ["Capodanno", "Jan 1, 2023 00:00:00"]
-var COMPLEANNO_VECIENZO = ["Vincenzo", "Jun 11, 2023 00:00:00"]
-var COMPLEANNO_SPAGNOLO = ["Spagnolo", "May 10, 2023 00:00:00"]
-var COMPLEANNO_PEDONE = ["Pedone", "Jun 14, 2023 00:00:00"]
-var COMPLEANNO_GIARDINO = ["Giardino", "May 9, 2023 00: 00: 00"]
 
 var countDownDate1 = new Date(NATALE[1]).getTime();
 var countDownDate2 = new Date(COMPLEANNO[1]).getTime();
 var countDownDate8 = new Date(TRENTAANNI[1]).getTime();
 var countDownDate3 = new Date(CAPODANNO[1]).getTime();
 
-var countDownDate4 = new Date(COMPLEANNO_VECIENZO[1]).getTime();
-var countDownDate5 = new Date(COMPLEANNO_SPAGNOLO[1]).getTime();
-var countDownDate6 = new Date(COMPLEANNO_PEDONE[1]).getTime();
-var countDownDate7 = new Date(COMPLEANNO_GIARDINO[1]).getTime();
-
-
 var Natale = document.getElementById(NATALE[0])
 var Compleanno = document.getElementById(COMPLEANNO[0])
 var Trentaanni = document.getElementById(TRENTAANNI[0])
 var Capodanno = document.getElementById(CAPODANNO[0])
 
-var Vincenzo = document.getElementById(COMPLEANNO_VECIENZO[0])
-var Spagnolo = document.getElementById(COMPLEANNO_SPAGNOLO[0])
-var Pedone = document.getElementById(COMPLEANNO_PEDONE[0])
-var Giardino = document.getElementById(COMPLEANNO_GIARDINO[0])
 
 function countdown(finish_date, timer) {
 
@@ -43,7 +29,6 @@ function countdown(finish_date, timer) {
 
         timer.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-
         if (distance < 0) {
             clearInterval(x);
             timer.innerHTML = "ICO Has Ended";
@@ -56,7 +41,18 @@ countdown(countDownDate2, Compleanno)
 countdown(countDownDate8, Trentaanni)
 countdown(countDownDate3, Capodanno)
 
-countdown(countDownDate4, Vincenzo)
-countdown(countDownDate5, Spagnolo)
-countdown(countDownDate6, Pedone)
-countdown(countDownDate7, Giardino)
+var weekNumb = document.getElementById(weekNum)
+
+function week(weekNumber) {
+    currentDate = new Date();
+    startDate = new Date(currentDate.getFullYear(), 0, 1);
+    var days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
+
+    var weekNumber = Math.ceil(days / 7);
+    return weekNumber;
+}
+
+week(weekNumb)
+// Display the calculated result      
+// document.write("Week number of " + currentDate +
+//   " is :   " + weekNumber);
